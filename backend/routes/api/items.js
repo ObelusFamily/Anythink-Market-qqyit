bd27db45a7d423c8e4a4088193f48ed806a815dc
PRESTON HRUBES <preston.hrubes@traderphd.com>
2022-10-03 22:50:11 -0600

    also unknown

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -54,7 +54,7 @@ router.get("/", auth.optional, function(req, res, next) {
   }
 
   if (typeof req.query.title !== "undefined") {
-    query.tagList = { $in: [req.query.title] };
+    query.title = { $regex: req.query.title };
   }
 
 
